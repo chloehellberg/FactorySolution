16df947ebd4250185e2fde4fd3aad3c7c09ecc59
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-16 09:42:48 -0700

    Engineer index, details build out with cooresponding controller. Move to update edit and delete.

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+  private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+        return View(model);
+    }
+
+     public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+      .Include(engineer => engineer.Machines)
+      .ThenInclude(join => join.Machine)
+      .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+    
+    // public ActionResult Edit(int id)
+    // {
+    //   var thisFarm = _db.Farms.FirstOrDefault(farm => farm.FarmId == id);
+    //   return View(thisFarm);
+    // }
+
+    // [HttpPost]
+    // public ActionResult Edit(Farm farm)
+    // {
+    //   _db.Entry(farm).State = EntityState.Modified;
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+    
+    // [HttpPost]
+    // public ActionResult AddMember(Farm farm, int MemberId)
+    // {
+    //   if (MemberId != 0)
+    //   {
+    //    _db.FarmMember.Add(new FarmMember() { MemberId = MemberId, FarmId = farm.FarmId });
+    //    }
+    //    _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+
+    // [HttpPost]
+    // public ActionResult DeleteMember(int joinId)
+    // {
+    //     var joinEntry = _db.FarmMember.FirstOrDefault(entry => entry.FarmMemberId == joinId);
+    //     _db.FarmMember.Remove(joinEntry);
+    //     _db.SaveChanges();
+    //     return RedirectToAction("Index");
+    // }
+    //  public ActionResult Delete(int id)
+    // {
+    //   var thisFarm = _db.Farms.FirstOrDefault(farms => farms.FarmId == id);
+    //   return View(thisFarm);
+    // }
+
+    // [HttpPost, ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   var thisFarm = _db.Farms.FirstOrDefault(farms => farms.FarmId == id);
+    //   _db.Farms.Remove(thisFarm);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // } 
+  }
+}
