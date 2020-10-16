e9570318a3606d5e1f60674c28fd851c38ef20be
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-10-16 14:12:17 -0700

    Update to display -working once again.

--- Factory/Controllers/CompanyController.cs
+++ Factory/Controllers/CompanyController.cs
@@ -1,25 +1,25 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Factory.Models;
-// using System.Collections.Generic;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 
-// namespace Factory.Controllers
-// {
-//   public class CompanyController : Controller
-//   {
-//   private readonly FactoryContext _db;
+namespace Factory.Controllers
+{
+  public class CompanyController : Controller
+  {
+  private readonly FactoryContext _db;
 
-//     public CompanyController(FactoryContext db)
-//     {
-//       _db = db;
-//     }
+    public CompanyController(FactoryContext db)
+    {
+      _db = db;
+    }
 
-//     public ActionResult Index()
-//     {
-//       List<Engineer> model = _db.Engineers.ToList();
-//         return View(model);
-//     }
-//   }
-// }
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+        return View(model);
+    }
+  }
+}
